be9732f3378115d3e8ba16e4ba65c8781dd3bf4e
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-09-14 13:45:48 +0300

    completed assignment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+def parrot(input_string="Squawk!"):
+    print(input_string)  # Print the input string
+    return input_string  # Return the input string
+
+
+input_string = "Hello, I'm a parrot!"
+output = parrot(input_string)
+
+
+
+
+    
